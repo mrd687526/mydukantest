288cc5695e3d4bf87c9417db8649bb9baa58a167
[dyad] <mrd687526@gmail.com>
2025-06-29 21:28:25 -0700

    [dyad] Fixed image loading error by configuring hostname in next.config.ts - wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'scontent-*.xx.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dash.mydukan.app', // Added this hostname
       }
     ],
   },
